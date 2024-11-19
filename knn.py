a8d71364db92b8f6309d4db1964f4ae831114762
phonlaphat23 <crazyeazy.yy@gmail.com>
2024-11-19 16:23:31 +0700

    Update knn.py

--- knn.py
+++ knn.py
@@ -10,15 +10,15 @@ st.image("./img/photo.jpg")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-   st.header("Versicolor")
+   st.header("💛Versicolor")
    st.image("./img/iris1.jpg")
 
 with col2:
-   st.header("Verginiga")
+   st.header("💜Verginiga")
    st.image("./img/iris2.jpg")
 
 with col3:
-   st.header("Setosa")
+   st.header("🩷Setosa")
    st.image("./img/iris3.jpg")
 
 html_7 = """
